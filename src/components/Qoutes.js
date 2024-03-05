db4a1d6f827d4161ae808cdc10c027f09a4d5cd9
Amanulla Mulla <amanullamulla394@gmail.com>
2024-03-05 21:19:23 +0530

    share button On Twitter is added

--- src/components/Qoutes.js
+++ src/components/Qoutes.js
@@ -1,18 +1,25 @@
 import React from "react";
 import qoutesList from "../Quotes.json";
+import shareOnTwitter from "../components/shareOnTwitter";
+import { FaShareAlt } from "react-icons/fa";
 
 const Qoutes = () => {
   return (
     <>
       {qoutesList.map((quote, index) => (
         <div className="m-24" key={index}>
-          <div className="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full p-4 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
             <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">
               {quote.quote}
             </h5>
-            <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
-              {quote.author}
-            </p>
+            <div className="flex justify-around w-full mt-2">
+              <p className="text-base text-gray-500 sm:text-lg dark:text-gray-400">
+                {quote.author}
+              </p>
+              <button onClick={() => shareOnTwitter(quote)}>
+                <FaShareAlt className="text-blue-700 font-bold text-2xl" />
+              </button>
+            </div>
           </div>
         </div>
       ))}
